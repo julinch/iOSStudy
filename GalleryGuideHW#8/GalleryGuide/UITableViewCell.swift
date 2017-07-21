a8c45647b28d85d1ca37a5849cc686bf9a329950
Juliya Linchevskaya <julia.linch1@gmail.com>
2017-07-21 14:59:05 +0300

    Removed her projects

--- GalleryGuideHW#8/GalleryGuide/UITableViewCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UITableViewCell.swift
-//  GalleryGuide
-//
-//  Created by Juliya on 26.06.17.
-//  Copyright © 2017 Juliya. All rights reserved.
-//
-
-import Foundation
--- GalleryProject/GalleryGuide/GalleryGuide/EventModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EventModel.swift
-//  GalleryGuide
-//
-//  Created by Juliya on 27.05.17.
-//  Copyright © 2017 Juliya. All rights reserved.
-//
-
-import Foundation
